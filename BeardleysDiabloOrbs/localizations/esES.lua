2cc3e5beedf76bf10642389bed04b3645c1940d1
Kulturnilpferd <37019912+Kulturnilpferd@users.noreply.github.com>
2019-03-10 16:58:13 +0100

    Add files via upload

---
+++ BeardleysDiabloOrbs/localizations/esES.lua
@@ -0,0 +1,130 @@
+if GetLocale() == "esES" or GetLocale() == "esMX" then
+	BD_ADDON_NO = "No"
+	BD_ADDON_YES = "Sí"
+	BD_BAR_SCALE = "Escala de la barra"
+	BD_BEAR = "Oso "
+	BD_CAT = "Gato"
+	BD_CHOOSE = "Elige uno..."
+	BD_CLOSE_BUTTON = "Cerrar"
+	BD_COMBAT = "En Combate"
+	BD_COMBO_ORB_SCALE = "Escala de Orbe de Combo"
+	BD_COMMIT = "Aplicar cambios"
+	BD_DEAD_TEXT = "Muerto"
+	BD_DEFAULT = "Por Defecto"
+	BD_FONT_CHOICE = "Fuente de Orbe"
+	BD_HELPMESSAGE_HEADLINE1 = "----Diablo Orbs de Bradley----"
+	BD_HELPMESSAGE_HEADLINE2 = "Comandos de Consola"
+	BD_HELPMESSAGE_TEXT1 = "/bdo menu -> Mostrar/Ocultar menu"
+	BD_HELPMESSAGE_TEXT2 = "/bdo enable -> Muestra los Orbes"
+	BD_HELPMESSAGE_TEXT3 = "/bdo disable -> Oculta los Orbes"
+	BD_HELPMESSAGE_TEXT4 = "/bdo & /bdo help -> Muestra este mensaje"
+	BD_LEFT_ORB_BUTTON = "Orbe Izquierdo"
+	BD_LOCK_WINDOWS = "Bloquear ventanas"
+	BD_MOONKIN = "Lechuzo"
+	BD_ORB_CONFIGURATION = "Configuración de Orbe"
+	BD_ORB_FILL = "Rellenar"
+	BD_ORB_FILLING = "Rellenar\nTextura"
+	BD_ORB_PERCENT = "Porcentaje"
+	BD_ORB_PERCENTAGE = "Mostrar\nPorcentaje"
+	BD_ORB_SCALE = "Escala de Orbe"
+	BD_ORB_SWIRL = "Remolino"
+	BD_ORB_TEXTURE = "Textura\nde Remolino"
+	BD_ORB_VALUE = "Valor"
+	BD_ORB_VALUES = "Mostrar\nValor"
+	BD_PET_ORB_SCALE = "Escala de Orbe de Mascota"
+	BD_RELOAD_REQUIRED = "Requiere\nreiniciar UI"
+	BD_RESET_WINDOWS = "Reiniciar Ventana"
+	BD_RIGHT_ORB_BUTTON = "Orbe Derecho"
+	BD_SET_TRUNCATED = "Formato de Valores"
+	BD_SHOW_ABSORB = "Mostrar\nAbsorción"
+	BD_SHOW_ARTWORK = "Mostrar Diseño"
+	BD_SHOW_BARIMAGE = "Mostrar Arte\nde barra de Acción"
+	BD_SHOW_COMBAT_ORB = "Mostrar Orbe\nde Combate"
+	BD_SHOW_COMBO_PERCENT = "Porcentaje de Orbe\nde Combo"
+	BD_SHOW_COMBO_TRACKER = "Mostrar Rastreador\nde Poder"
+	BD_SHOW_COMBO_VALUES = "Valores del Orbe\nde Combos"
+	BD_SHOW_MENU = "Abrir menu"
+	BD_SHOW_MINIMAP = "Mostrar icono de minimapa"
+	BD_SHOW_PETORB = "Mostrar Orbe\nde Mascota"
+	BD_SHOW_PETORB_PERCENT = "Mostrar Porcentaje\nde Mascota"
+	BD_SHOW_PETORB_VALUES = "Mostrar Valores\nde Mascota"
+	BD_SHOW_PLAYERFRAME = "Mostrar Marco\nde Jugador"
+	BD_SHOW_RAREELITE_FRAME = "Mostrar Marco\nde Rare Elite"
+	BD_SHOW_TARGET_FRAME = "Mostrar Marco\nde Objetivo"
+	BD_SHOW_TARGET_ORB = "Mostrar Orbe\nde Objetivo"
+	BD_SHOW_TARGET_PERCENT = "Porcentaje de Orbe\nde Objetivo"
+	BD_SHOW_TARGET_VALUE = "Valor del Orbe\nde Objetivo"
+	BD_SMOOTH_ANIMATION = "Animación Suave"
+	BD_SWAP_COMBO_ORB = "Intercambiar Orbe\nde Combo-Mana"
+	BD_TARGET_ORB_SCALE = "Escala del Orbe de Objetivo"
+	BD_TOOLTIP_ANIMATEORBVALUES = "Cuando se activa, las animaciones del orbe\nson mostradas de forma mas liquidas\npero también con un retraso mínimo."
+	BD_TOOLTIP_COMBOORBSCALESLIDER = "Cambiar el tamaño del Orbe de Combo"
+	BD_TOOLTIP_FILLBUTTON = "Cambiar el color de Relleno del Orbe"
+	BD_TOOLTIP_FILLTEXTURES = "Selector de Fondo del Orbe"
+	BD_TOOLTIP_HEALTHORBOPTIONS = "Cambiar Normal/Orbe de Combate"
+	BD_TOOLTIP_HEALTHORBSCALESLIDER = "Cambiar el tamaño del orbe izquierdo"
+	BD_TOOLTIP_LOCKBDWINDOWS = "Bloquear los elemenos del UI BDO"
+	BD_TOOLTIP_MANAORBOPTIONS = "Cambio de forma/Selección de Orbe de Energía"
+	BD_TOOLTIP_MANAORBSCALESLIDER = "Cambiar el tamaño del Orbe derecho"
+	BD_TOOLTIP_ORBABSORB = "Muestra la animación de escudo en el orbe izquierdo si hay alguna aura de escudo activo en ti"
+	BD_TOOLTIP_ORBFONT = "Selector de fuente de Orbe"
+	BD_TOOLTIP_ORBPERCENTAGE = "Muestra el estatus como orcentaje"
+	BD_TOOLTIP_ORBSCALESLIDER = "Cambiar el tamaño del orbe izquierdo y derecho juntos"
+	BD_TOOLTIP_ORBVALUE = "Mostrar el estatus como valor numérico"
+	BD_TOOLTIP_PERCENTAGEBUTTON = "Cambiar porcentaje de color del Orbe"
+	BD_TOOLTIP_PETORBSCALESLIDER = "Cambiar el tamaño del Orbe de Mascota"
+	--Translation missing 
+	BD_TOOLTIP_RESETBDWINDOWS = "Reset all Actionbar- & Orbpositions"
+	--Translation missing 
+	BD_TOOLTIP_ROTATIONTEXTURES = "Orb animation selection"
+	--Translation missing 
+	BD_TOOLTIP_SCALESLIDER = "Change Actionbar size"
+	--Translation missing 
+	BD_TOOLTIP_SHOWARTWORK = "Show up Demon and Angel artwork to the left and right on the Orb"
+	--Translation missing 
+	BD_TOOLTIP_SHOWBDTARGETORB = "Show TargetOrb\nAttention: The UI is reloaded"
+	--Translation missing 
+	BD_TOOLTIP_SHOWBDTARGETORBPERCENTAGE = "Show status as percentage"
+	--Translation missing 
+	BD_TOOLTIP_SHOWBDTARGETORBVALUE = "Display status as a numeric value"
+	--Translation missing 
+	BD_TOOLTIP_SHOWBLIZZPLAYERFRAME = "Show original Playerframe"
+	--Translation missing 
+	BD_TOOLTIP_SHOWCOMBOPERCENT = "Show status as percentage"
+	--Translation missing 
+	BD_TOOLTIP_SHOWCOMBOVALUES = "Display status as a numeric value"
+	--Translation missing 
+	BD_TOOLTIP_SHOWORBELITEFRAME = "Show up Rare- & Elite artwork on target Orb"
+	--Translation missing 
+	BD_TOOLTIP_SHOWPETORBPERCENTAGES = "Show status as percentage"
+	--Translation missing 
+	BD_TOOLTIP_SHOWPETORBVALUES = "Display status as a numeric value"
+	--Translation missing 
+	BD_TOOLTIP_SHOWTARGETFRAME = "Displays the original target window\nAttention: The UI is reloaded"
+	--Translation missing 
+	BD_TOOLTIP_SWAPCOMBOVALUES = "Swap combo Orb and right Orb with each other"
+	--Translation missing 
+	BD_TOOLTIP_SWIRLBUTTON = "Change Orb Animation color"
+	--Translation missing 
+	BD_TOOLTIP_TARGETORBSCALESLIDER = "Change target Orb size"
+	--Translation missing 
+	BD_TOOLTIP_TRACKCOMBAT = "Change Orbcolor during fight"
+	--Translation missing 
+	BD_TOOLTIP_TRUNCATEDVALUES = "Values are displayed rounded and truncated"
+	--Translation missing 
+	BD_TOOLTIP_USEBARIMAGE = "Display the Actionbar background"
+	--Translation missing 
+	BD_TOOLTIP_USEPETORB = "Pet Orb will be shown"
+	--Translation missing 
+	BD_TOOLTIP_USEPOWERTRACKER = "When second energy available\ndisplay it on a second Orb"
+	--Translation missing 
+	BD_TOOLTIP_VALUEBUTTON = "Change Orb value color"
+	BDINFIGT_MESSAGE = "Beardley's Diablo Orbs: El menu no se puede abrir durante las peleas."
+	--Translation missing 
+	BDMINIMAP_HEADLINE = "Beardley's Diablo Orbs"
+	BDMINIMAP_TEXT1 = "[Click Izquierdo] para abrir el menu"
+	BDMINIMAP_TEXT2 = "[Shift + Click Izquierdo] para mover: Los orbes y el fondo de las barras."
+	BDMINIMAP_TEXT3 = "[Shift + Click Izquierdo] para mover las barras de accion del lado derecho."
+	--Translation missing 
+	RESET_BD_WINDOWS_TEXT = "Beardley's Diablo Orbs: Reset all window positions of BDO?"
+end
---
+++ BeardleysDiabloOrbs/localizations/ptBR.lua
@@ -0,0 +1,179 @@
+if GetLocale() == "ptBR" then
+	BD_ADDON_NO = "Não"
+	BD_ADDON_YES = "Sim"
+	BD_BAR_SCALE = "Escala da Barra"
+	BD_BEAR = "Urso"
+	BD_CAT = "Felino"
+	BD_CHOOSE = "Escolha um..."
+	BD_CLOSE_BUTTON = "Fechar"
+	BD_COMBAT = "Em combate"
+	BD_COMBO_ORB_SCALE = "Escala do Orb de Combo"
+	BD_COMMIT = "Aplicar mudanças"
+	BD_DEAD_TEXT = "MORTO"
+	BD_DEFAULT = "Padrão"
+	BD_FONT_CHOICE = "Fonte da esfera"
+	BD_HELPMESSAGE_HEADLINE1 = "----Beardley's Diablo Orbs----"
+	BD_HELPMESSAGE_HEADLINE2 = "Comandos de console"
+	BD_HELPMESSAGE_TEXT1 = "/bdo menu -> Mostrar/Esconder menu"
+	BD_HELPMESSAGE_TEXT2 = "/bdo enable -> Mostrar esferas"
+	BD_HELPMESSAGE_TEXT3 = "Esconder Esferas"
+	BD_HELPMESSAGE_TEXT4 = "/bdo & /bdo help -> Mostrar esta mensagem"
+	BD_LEFT_ORB_BUTTON = "Left orb"
+	BD_LOCK_WINDOWS = "Travar frames"
+	BD_MOONKIN = "Lumiscante"
+	BD_ORB_CONFIGURATION = "Configuração da Orb"
+	BD_ORB_FILL = "Preenchimento"
+	BD_ORB_FILLING = "Textura interna"
+	BD_ORB_PERCENT = "Porcentagem"
+	BD_ORB_PERCENTAGE = "Mostrar Porcentagem"
+	BD_ORB_SCALE = "Escala da Orb"
+	--Translation missing 
+	BD_ORB_SWIRL = "Swirl"
+	--Translation missing 
+	BD_ORB_TEXTURE = "       Swirl Texture"
+	BD_ORB_VALUE = "Valor"
+	--Translation missing 
+	BD_ORB_VALUES = "Show Value"
+	--Translation missing 
+	BD_PET_ORB_SCALE = "Pet Orb Scale"
+	--Translation missing 
+	BD_RELOAD_REQUIRED = "UI reload required"
+	--Translation missing 
+	BD_RESET_WINDOWS = "Reset     Windows"
+	--Translation missing 
+	BD_RIGHT_ORB_BUTTON = "Right Orb"
+	--Translation missing 
+	BD_SET_TRUNCATED = "Format Values"
+	--Translation missing 
+	BD_SHOW_ABSORB = "Show Absorb"
+	--Translation missing 
+	BD_SHOW_ARTWORK = "Show Artwork"
+	--Translation missing 
+	BD_SHOW_BARIMAGE = "Show Actiobar Art"
+	--Translation missing 
+	BD_SHOW_COMBAT_ORB = "Show Combat Orb"
+	--Translation missing 
+	BD_SHOW_COMBO_PERCENT = "Combo Orb Percentage"
+	--Translation missing 
+	BD_SHOW_COMBO_TRACKER = "Show Power Tracker"
+	--Translation missing 
+	BD_SHOW_COMBO_VALUES = "Combo Orb Values"
+	--Translation missing 
+	BD_SHOW_MENU = "Open menu"
+	--Translation missing 
+	BD_SHOW_MINIMAP = "Show minimap icon"
+	--Translation missing 
+	BD_SHOW_PETORB = "Show Pet Orb"
+	--Translation missing 
+	BD_SHOW_PETORB_PERCENT = "Show Pet Percentage"
+	--Translation missing 
+	BD_SHOW_PETORB_VALUES = "Show Pet Values"
+	--Translation missing 
+	BD_SHOW_PLAYERFRAME = "Show Player Frame"
+	--Translation missing 
+	BD_SHOW_RAREELITE_FRAME = "Show Rare/Elite frame"
+	--Translation missing 
+	BD_SHOW_TARGET_FRAME = "Show Target Frame"
+	--Translation missing 
+	BD_SHOW_TARGET_ORB = "Show Target Orb"
+	--Translation missing 
+	BD_SHOW_TARGET_PERCENT = "Target Orb Percentage"
+	--Translation missing 
+	BD_SHOW_TARGET_VALUE = "Target Orb Value"
+	--Translation missing 
+	BD_SMOOTH_ANIMATION = "Smooth Animate"
+	--Translation missing 
+	BD_SWAP_COMBO_ORB = "Swap Mana-Combo Orb"
+	--Translation missing 
+	BD_TARGET_ORB_SCALE = "Target Orb Scale"
+	--Translation missing 
+	BD_TOOLTIP_ANIMATEORBVALUES = "When activated, the orb animations are displayed more liquid but also minimally delayed"
+	--Translation missing 
+	BD_TOOLTIP_COMBOORBSCALESLIDER = "Change combo Orb size"
+	--Translation missing 
+	BD_TOOLTIP_FILLBUTTON = "Change Orb filling color"
+	--Translation missing 
+	BD_TOOLTIP_FILLTEXTURES = "Orb background selection"
+	--Translation missing 
+	BD_TOOLTIP_HEALTHORBOPTIONS = "Change Normal/Combat Orb"
+	--Translation missing 
+	BD_TOOLTIP_HEALTHORBSCALESLIDER = "Change left Orb size"
+	--Translation missing 
+	BD_TOOLTIP_LOCKBDWINDOWS = "Lock BDO's UI Elements"
+	--Translation missing 
+	BD_TOOLTIP_MANAORBOPTIONS = "Shapeshift/Enery Orb selection"
+	--Translation missing 
+	BD_TOOLTIP_MANAORBSCALESLIDER = "Change right Orb size"
+	--Translation missing 
+	BD_TOOLTIP_ORBABSORB = "Shows shield animation on left Orb if there is any active shield buff on you"
+	--Translation missing 
+	BD_TOOLTIP_ORBFONT = "Orb font selection"
+	--Translation missing 
+	BD_TOOLTIP_ORBPERCENTAGE = "Show status as percentage"
+	--Translation missing 
+	BD_TOOLTIP_ORBSCALESLIDER = "Change Left and Right Orb Size together"
+	--Translation missing 
+	BD_TOOLTIP_ORBVALUE = "Display status as a numeric value"
+	--Translation missing 
+	BD_TOOLTIP_PERCENTAGEBUTTON = "Change Orb percentage color"
+	--Translation missing 
+	BD_TOOLTIP_PETORBSCALESLIDER = "Chage pet Orb size"
+	--Translation missing 
+	BD_TOOLTIP_RESETBDWINDOWS = "Reset all Actionbar- & Orbpositions"
+	--Translation missing 
+	BD_TOOLTIP_ROTATIONTEXTURES = "Orb animation selection"
+	--Translation missing 
+	BD_TOOLTIP_SCALESLIDER = "Change Actionbar size"
+	--Translation missing 
+	BD_TOOLTIP_SHOWARTWORK = "Show up Demon and Angel artwork to the left and right on the Orb"
+	--Translation missing 
+	BD_TOOLTIP_SHOWBDTARGETORB = "Show TargetOrb Attention: The UI is reloaded"
+	--Translation missing 
+	BD_TOOLTIP_SHOWBDTARGETORBPERCENTAGE = "Show status as percentage"
+	--Translation missing 
+	BD_TOOLTIP_SHOWBDTARGETORBVALUE = "Display status as a numeric value"
+	--Translation missing 
+	BD_TOOLTIP_SHOWBLIZZPLAYERFRAME = "Show original Playerframe"
+	--Translation missing 
+	BD_TOOLTIP_SHOWCOMBOPERCENT = "Show status as percentage"
+	--Translation missing 
+	BD_TOOLTIP_SHOWCOMBOVALUES = "Display status as a numeric value"
+	--Translation missing 
+	BD_TOOLTIP_SHOWORBELITEFRAME = "Show up Rare- & Elite artwork on target Orb"
+	--Translation missing 
+	BD_TOOLTIP_SHOWPETORBPERCENTAGES = "Show status as percentage"
+	--Translation missing 
+	BD_TOOLTIP_SHOWPETORBVALUES = "Display status as a numeric value"
+	--Translation missing 
+	BD_TOOLTIP_SHOWTARGETFRAME = "Displays the original target window Attention: The UI is reloaded"
+	--Translation missing 
+	BD_TOOLTIP_SWAPCOMBOVALUES = "Swap combo Orb and right Orb with each other"
+	--Translation missing 
+	BD_TOOLTIP_SWIRLBUTTON = "Change Orb Animation color"
+	--Translation missing 
+	BD_TOOLTIP_TARGETORBSCALESLIDER = "Change target Orb size"
+	--Translation missing 
+	BD_TOOLTIP_TRACKCOMBAT = "Change Orbcolor during fight"
+	--Translation missing 
+	BD_TOOLTIP_TRUNCATEDVALUES = "Values are displayed rounded and truncated"
+	--Translation missing 
+	BD_TOOLTIP_USEBARIMAGE = "Display the Actionbar background"
+	--Translation missing 
+	BD_TOOLTIP_USEPETORB = "Pet Orb will be shown"
+	--Translation missing 
+	BD_TOOLTIP_USEPOWERTRACKER = "When second energy available display it on a second Orb"
+	--Translation missing 
+	BD_TOOLTIP_VALUEBUTTON = "Change Orb value color"
+	--Translation missing 
+	BDINFIGT_MESSAGE = "Beardley's Diablo Orbs: Menu can not be opened in the fight"
+	--Translation missing 
+	BDMINIMAP_HEADLINE = "Beardley's Diablo Orbs"
+	--Translation missing 
+	BDMINIMAP_TEXT1 = "[Leftclick] to open the menu"
+	--Translation missing 
+	BDMINIMAP_TEXT2 = "[Shift + Leftclick] to move Orbs / Bar background / Powerframe"
+	--Translation missing 
+	BDMINIMAP_TEXT3 = "[Shift + Leftclick (between the buttons)] to move the actionbars on right side"
+	--Translation missing 
+	RESET_BD_WINDOWS_TEXT = "Beardley's Diablo Orbs: Reset all window positions of BDO?"
+end
